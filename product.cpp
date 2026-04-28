f3378e7b6e9da8465f7828dffd6e5b6be581f90c
Denay2468 <denayaung13@gmail.com>
2026-04-28 09:38:50 +0630

    create display function for product class

--- product.cpp
+++ product.cpp
@@ -10,4 +10,12 @@ class Product
         int quantity;
         string category;
         string productionDate;
+
+        void display()
+        {
+            cout << "Name: " << Name << endl;
+            cout << "Quantity: " << quantity << endl;
+            cout << "Category: " << category << endl;
+            cout << "Production Date: " << productionDate << endl;
+        }
 };
