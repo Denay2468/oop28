6a5953b3570f94fa8974fa2eb7e050f76f40dc55
Denay2468 <denayaung13@gmail.com>
2026-04-28 09:33:44 +0630

    create class and public variable inside

--- product.cpp
+++ product.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+
+class Product
+{
+    public:
+        string Name;
+        int quantity;
+        string category;
+        string productionDate;
+};
